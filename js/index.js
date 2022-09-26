fc4722547d6a020f8af805d54c628313bdc2edb2
Holly Tunison <hollytunison@gmail.com>
2022-09-26 19:36:48 -0400

    added ht logo

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-document.querySelector("#scroll-down").addEventListener("click", () => {
-  window.scrollTo({
-    top: document.querySelector("#about-me").offsetTop - 20,
-  });
-});
 
 document.querySelector("#toggle-theme").addEventListener("click", () => {
   document.documentElement.classList.toggle("light-theme");
